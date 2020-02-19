03255cd8e7dd73055116062cee23f2c9b11e57d0
Moni <nikita2002@live.com.ar>
2020-02-19 04:48:40 -0300

    Html/Css de Carrito/Pago y vinculacion con resto htmls

---
+++ proyectoIntegradorPracticaJava/src/test/java/com/proyecto/proyectoIntegradorPracticaJava/ProyectoIntegradorPracticaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectoIntegradorPracticaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorPracticaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
