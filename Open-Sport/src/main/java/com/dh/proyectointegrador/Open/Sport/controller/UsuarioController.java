f864d4d0d827aa10923836b28b5ee0a71969046d
Silvina <silvinapan@hotmail.com>
2020-02-25 22:43:12 -0300

    iteracion para mostrar en una tabla los usuarios registrados

--- Open-Sport/src/main/java/com/dh/proyectointegrador/Open/Sport/controller/UsuarioController.java
+++ Open-Sport/src/main/java/com/dh/proyectointegrador/Open/Sport/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.dh.proyectointegrador.Open.Sport.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.dh.proyectointegrador.Open.Sport.repository.usuarioJpaRepository;
+import com.dh.proyectointegrador.Open.Sport.model.Producto;
 import com.dh.proyectointegrador.Open.Sport.model.Usuario;
 
 
@@ -21,5 +25,12 @@ public class UsuarioController {
 	public String registrarUsuario(Usuario unUsuario) {
 		usuarioJpaRepository.save(unUsuario);
 		return "/home/home";
+	}
+	
+	@GetMapping("usuarios")
+	public String listarUSuarios(Model model) {
+		List<Usuario> listaDeUsuarios = usuarioJpaRepository.findAll();
+		model.addAttribute("listaDeUsuarios", listaDeUsuarios);
+		return "/registro/listaUsuarios";
 	}
 }
