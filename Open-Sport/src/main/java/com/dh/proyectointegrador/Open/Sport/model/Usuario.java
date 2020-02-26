e0f12c9739f396ec5fd4fb0e71a12e6e07b1d263
Silvina <silvinapan@hotmail.com>
2020-02-26 16:37:00 -0300

    tabla intermedia usuarios_productos. Sera la compra con l aque se genere la factura al cliente

--- Open-Sport/src/main/java/com/dh/proyectointegrador/Open/Sport/model/Usuario.java
+++ Open-Sport/src/main/java/com/dh/proyectointegrador/Open/Sport/model/Usuario.java
@@ -2,11 +2,14 @@ package com.dh.proyectointegrador.Open.Sport.model;
 
 
 
-import javax.persistence.Column;
+import java.util.List;
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 /**
@@ -26,48 +29,47 @@ public class Usuario {
 	private String apellido;
 	private String nombre;
 	private Integer dni;
+	@ManyToMany
+	private List<Producto> productos;
 	
-	public Usuario() {
-		
-	}
-
-	public Usuario(Integer iD, String apellido, String nombre, Integer dni) {
-		ID = iD;
+	public Usuario(Integer iD, String apellido, String nombre, Integer dni, List<Producto> productos) {
+		this.ID = iD;
 		this.apellido = apellido;
 		this.nombre = nombre;
 		this.dni = dni;
+		this.productos = productos;
 	}
-
+	
 	public Integer getID() {
 		return ID;
 	}
-
 	public void setID(Integer iD) {
 		ID = iD;
 	}
-
 	public String getApellido() {
 		return apellido;
 	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public Integer getDni() {
 		return dni;
 	}
-
 	public void setDni(Integer dni) {
 		this.dni = dni;
 	}
-
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	
 }
